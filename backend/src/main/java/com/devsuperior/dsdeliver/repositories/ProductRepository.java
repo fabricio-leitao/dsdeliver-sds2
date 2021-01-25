402dd7e496ea79d4ae4b383e4c4d707742965d83
Fabricio Ramos <fabricio-beco@hotmail.com>
2021-01-25 16:54:58 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsdeliver.repositories;
+
+import java.util.List;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  List<Product> findAllByOrderByNameAsc();
+}
