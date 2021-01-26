2bbab97c508593395f0ed38a61578004c3389455
Fabricio Ramos <fabricio-beco@hotmail.com>
2021-01-26 11:34:15 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
